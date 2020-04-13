04e7dff803af5ad9ba092205c9dcc0c7a684554c
Marina Gordynets <timary99@gmail.com>
2020-04-13 15:54:48 +0300

    feat: add moving icon menu

--- burger-menu/src/app.js
+++ burger-menu/src/app.js
@@ -2,16 +2,24 @@ import "./styles/index.css";
 import './styles/burger-btn.css';
 import './styles/right-gorizontal-menu.css';
 import './styles/right-vertical-menu.css';
+import './styles/icon-menu.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
 
 const btns = document.getElementsByClassName('menu-btn');
 const menu = document.getElementsByClassName('menu');
+const menu_icon = document.getElementsByClassName('menu-icon')[0];
 
  for(let i = 0; i < btns.length; i++) {
-    console.log('btn');
     const eventClick = btns[i].addEventListener('click', function (e) {
         e.preventDefault();
         btns[i].classList.toggle('menu-btn_active');
-        menu[i].classList.toggle('menu_active');
+
+        if(i < 2) {
+            menu[i].classList.toggle('menu_active');}
+        else {
+            menu_icon.classList.toggle('active');
+        }
     });
 
      const eventLinkClick = menu[i].addEventListener('click', function (e) {
