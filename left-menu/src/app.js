963fd4afbbd9fcdc25fd775721fd710e47fdb822
Marina Gordynets <timary99@gmail.com>
2020-04-16 17:59:47 +0300

    feat add burger menu button

--- left-menu/src/app.js
+++ left-menu/src/app.js
@@ -1,11 +1,14 @@
-import './index.css';
+import './styles/index.css';
+import './styles/menu-btn.css';
 
 const menu_btn = document.getElementsByClassName('menu-btn')[0];
 const menu = document.getElementsByClassName('menu')[0];
 const content = document.getElementsByClassName('content')[0];
+const menu_btn_burger = document.getElementsByClassName('menu-btn-burger')[0];
 
 const openMenuEvent = menu_btn.addEventListener('click', function (e) {
     e.preventDefault();
     menu.classList.toggle('menu-active');
     content.classList.toggle('content-active');
+    menu_btn_burger.classList.toggle('menu-btn-burger-active');
 });
