dd502e1f8ab40bc6dfee96106e515e4f37b61729
Marina Gordynets <timary99@gmail.com>
2020-10-01 13:13:05 +0300

    fix: fix item select

--- multiple-select-table/script.js
+++ multiple-select-table/script.js
@@ -1,6 +1,8 @@
 const table = document.querySelector('#table');
 
 table.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target.classList);
     event.target.classList.toggle('selected');
     const button = event.target.dataset.selector;
     const toggleSelected = button => button.classList.toggle('selected');
@@ -9,33 +11,39 @@ table.addEventListener('click', (event) => {
         const buttons = document.querySelectorAll('[data-align]');
         buttons.forEach(removeSelected);
     }
-    deSelectAllButtons();
     switch (button) {
         case 'left':
+            deSelectAllButtons();
             const left_buttons = document.querySelectorAll('[data-align="left"]');
             left_buttons.forEach(toggleSelected);
             break;
         case 'center':
+            deSelectAllButtons();
             const center_buttons = document.querySelectorAll('[data-align="center"]');
             center_buttons.forEach(toggleSelected);
             break;
         case 'right':
+            deSelectAllButtons();
             const right_buttons = document.querySelectorAll('[data-align="right"]');
             right_buttons.forEach(toggleSelected);
             break;
         case 'start':
+            deSelectAllButtons();
             const start_buttons = document.querySelectorAll('[data-justify="start"]');
             start_buttons.forEach(toggleSelected);
             break;
         case 'middle':
+            deSelectAllButtons();
             const middle_buttons = document.querySelectorAll('[data-justify="middle"]');
             middle_buttons.forEach(toggleSelected);
             break;
         case 'end':
+            deSelectAllButtons();
             const end_buttons = document.querySelectorAll('[data-justify="end"]');
             end_buttons.forEach(toggleSelected);
             break;
         case 'all':
+            deSelectAllButtons();
             const all_buttons = document.querySelectorAll('[data-justify]');
             all_buttons.forEach(toggleSelected);
             break;
